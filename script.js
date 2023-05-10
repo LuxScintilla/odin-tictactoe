94eabe9de1b2630dc2a9a34d1e6a745cea72d485
LuxScintilla <catherina.ea@gmail.com>
2023-05-10 23:01:21 +0100

    Add working JS for board basis

--- script.js
+++ script.js
@@ -6,57 +6,69 @@ function showGame() {
   startButton.style.display = "none";
 }
 
+const CreatePlayer = function (name, mark, color) {
+  this.name = name;
+  this.mark = mark;
+  this.color = color;
+};
+const playerOne = new CreatePlayer("Player 1", "X", "#ff3c7d");
+const playerTwo = new CreatePlayer("Player 2", "O", "#00ebcb");
+
 const gameBoard = (function () {
   const cell = document.querySelectorAll(".cell");
 
-  const CreatePlayer = function (name, mark, color) {
-    this.name = name;
-    this.mark = mark;
-    this.color = color;
-  };
-  const playerOne = new CreatePlayer("Player 1", "X", "#ff3c7d");
-  const playerTwo = new CreatePlayer("Player 2", "O", "#00ebcb");
+  let turn = 1;
 
-  turn = 1;
-
-  let boardArray = ["", "", "", "", "", "", "", "", ""];
+  markArray = ["", "", "", "", "", "", "", "", ""];
 
   const winCombo = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
 
-  const checkWin = function () {
-    for (let i = 0; i < cell.length; i++) {
-      if (TO BE CONTINUED HERE!!!!) {
-        
+  function checkCombo() {
+    for (let i = 0; i < winCombo.length; i++) {
+      if (
+        cell[winCombo[i][0]].textContent === "X" &&
+        cell[winCombo[i][1]].textContent === "X" &&
+        cell[winCombo[i][2]].textContent === "X"
+      ) {
+        console.log("Player 1 is the Winner!");
+      } else if (
+        cell[winCombo[i][0]].textContent === "O" &&
+        cell[winCombo[i][1]].textContent === "O" &&
+        cell[winCombo[i][2]].textContent === "O"
+      ) {
+        console.log("Player 2 is the Winner!");
+      } else if (!markArray.includes("")) {
+        console.log("It is a TIE!");
       }
     }
-  };
+  }
 
-  const placeMarker = function () {
+  function placeMarker() {
     for (let i = 0; i < cell.length; i++) {
       cell[i].addEventListener("click", function () {
         if (cell[i].textContent === "" && turn === 1) {
-          cell[i].style.color = playerOne.color;
           cell[i].textContent = playerOne.mark;
-          boardArray[i] = playerOne.mark;
+          markArray[i] = playerOne.mark;
+          checkCombo();
           turn = 2;
         } else if (cell[i].textContent === "" && turn === 2) {
-          cell[i].style.color = playerTwo.color;
           cell[i].textContent = playerTwo.mark;
-          boardArray[i] = playerTwo.mark;
+          markArray[i] = playerTwo.mark;
+          checkCombo();
           turn = 1;
         }
       });
     }
-  };
+  }
 
   return { placeMarker };
 })();
